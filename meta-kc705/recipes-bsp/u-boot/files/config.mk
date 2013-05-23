c93171bbbba4b8698d710807c5f1810bd49855df
Nathan Rossi <nathan.rossi@xilinx.com>
2013-05-23 11:31:26 +1000

    meta-kc705: kc705-trd: Update configuration to match exact hardware
    * Update configuration files and DTS to match the exact Targeted Reference
      Design configuration.
    
    Signed-off-by: Nathan Rossi <nathan.rossi@xilinx.com>

--- meta-kc705/recipes-bsp/u-boot/files/config.mk
+++ meta-kc705/recipes-bsp/u-boot/files/config.mk
@@ -1,9 +1,9 @@
 PLATFORM_CPPFLAGS += -mxl-pattern-compare
 PLATFORM_CPPFLAGS += -mxl-barrel-shift
-PLATFORM_CPPFLAGS += -mno-xl-soft-div
-PLATFORM_CPPFLAGS += -mxl-multiply-high
+PLATFORM_CPPFLAGS += -mxl-soft-div
+PLATFORM_CPPFLAGS += -mno-xl-multiply-high
 PLATFORM_CPPFLAGS += -mno-xl-soft-mul
 PLATFORM_CPPFLAGS += -mcpu=v8.50.a
 
-TEXT_BASE = 0x87c00000
-CONFIG_SYS_TEXT_BASE = 0x87c00000
+TEXT_BASE = 0xbfc00000
+CONFIG_SYS_TEXT_BASE = 0xbfc00000
--- meta-kc705/recipes-bsp/u-boot/files/xparameters.h
+++ meta-kc705/recipes-bsp/u-boot/files/xparameters.h
@@ -4,15 +4,15 @@
 #define XILINX_FSL_NUMBER	0
 #define XILINX_USE_ICACHE	1
 #define XILINX_USE_DCACHE	1
-#define XILINX_DCACHE_BYTE_SIZE	16384
+#define XILINX_DCACHE_BYTE_SIZE	8192
 
-/* Interrupt controller is microblaze_0_intc */
-#define XILINX_INTC_BASEADDR		0x41200000
-#define XILINX_INTC_NUM_INTR_INPUTS	5
+/* Interrupt controller is interrupt_cntlr */
+#define XILINX_INTC_BASEADDR		0x40100000
+#define XILINX_INTC_NUM_INTR_INPUTS	8
 
-/* Timer pheriphery is system_timer */
-#define XILINX_TIMER_BASEADDR	0x41c00000
-#define XILINX_TIMER_IRQ	2
+/* Timer pheriphery is dual_timer_counter */
+#define XILINX_TIMER_BASEADDR	0x40300000
+#define XILINX_TIMER_IRQ	3
 
 /* System Timer Clock Frequency */
 #define XILINX_CLOCK_FREQ	100000000
@@ -21,26 +21,28 @@
 #define XILINX_UART16550
 #define XILINX_UART16550_BASEADDR	0x40400000
 #define XILINX_UART16550_CLOCK_HZ	100000000
-#define XILINX_UARTLITE_BASEADDR	0x41400000
+#define XILINX_UARTLITE_BASEADDR	0x40200000
 #define CONFIG_CONS_INDEX		1
 
-/* IIC doesn't exist */
+/* IIC pheriphery is iic_eeprom */
+#define XILINX_IIC_0_BASEADDR	0x40a00000
+#define XILINX_IIC_0_FREQ	100000
+#define XILINX_IIC_0_BIT	0
 
-/* GPIO is dip_switches_8bits*/
-#define XILINX_GPIO_BASEADDR	0x40060000
+/* GPIO doesn't exist */
 
 /* SDIO doesn't exist */
 
 /* Main Memory is ddr3_sdram */
 #define XILINX_RAM_START	0x80000000
-#define XILINX_RAM_SIZE		0x08000000
+#define XILINX_RAM_SIZE		0x40000000
 
 /* Flash Memory is linear_flash */
-#define XILINX_FLASH_START	0xa0000000
+#define XILINX_FLASH_START	0x48000000
 #define XILINX_FLASH_SIZE	0x08000000
 
 /* Sysace doesn't exist */
 
-/* Ethernet controller is ethernet */
-#define XILINX_AXIEMAC_BASEADDR			0x44f40000
-#define XILINX_AXIDMA_BASEADDR			0x41e00000
+/* Ethernet controller is soft_ethernet_mac */
+#define XILINX_AXIEMAC_BASEADDR			0x50100000
+#define XILINX_AXIDMA_BASEADDR			0x50000000
