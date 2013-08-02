d9617006376d73e37f1db17857ee06c24e6b7c03
Nathan Rossi <nathan.rossi@xilinx.com>
2013-08-02 12:41:14 +1000

    meta-*: Remove deprecated sublayers
    * The machines supported in the sublayer BSPs are now supported in the base
      meta-xilinx layer, these sublayers are no longer required.
    * Relevant documentation has also been relocated into meta-xilinx/docs/*.
    
    Signed-off-by: Nathan Rossi <nathan.rossi@xilinx.com>

--- meta-kc705/recipes-bsp/u-boot/files/config.mk
+++
@@ -1,9 +0,0 @@
-PLATFORM_CPPFLAGS += -mxl-pattern-compare
-PLATFORM_CPPFLAGS += -mxl-barrel-shift
-PLATFORM_CPPFLAGS += -mxl-soft-div
-PLATFORM_CPPFLAGS += -mno-xl-multiply-high
-PLATFORM_CPPFLAGS += -mno-xl-soft-mul
-PLATFORM_CPPFLAGS += -mcpu=v8.50.a
-
-TEXT_BASE = 0xbfc00000
-CONFIG_SYS_TEXT_BASE = 0xbfc00000
--- meta-kc705/recipes-bsp/u-boot/u-boot-xlnx_2013.01.bbappend
+++
@@ -1,6 +0,0 @@
-
-FILESEXTRAPATHS_append_kc705-trd := "${THISDIR}/files:"
-SRC_URI_append_kc705-trd = " \
-		file://config.mk \
-		file://xparameters.h \
-		"
