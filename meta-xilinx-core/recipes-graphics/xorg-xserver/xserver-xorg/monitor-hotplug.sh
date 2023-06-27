4c17247f4e5e186776903e5ff3effa523e7e5ee2
Sandeep Gundlupet Raju <sandeep.gundlupet-raju@amd.com>
2023-06-27 15:44:36 -0600

    xserver-xorg: Move monitor hotplug rules from meta-petalinux
    Signed-off-by: Sandeep Gundlupet Raju <sandeep.gundlupet-raju@amd.com>
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

---
+++ meta-xilinx-core/recipes-graphics/xorg-xserver/xserver-xorg/monitor-hotplug.sh
@@ -0,0 +1,50 @@
+#! /bin/sh
+
+# Copyright (C) 2018 Xilinx, Inc. All rights reserved.
+# Copyright (C) 2023 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Adapt this script to your needs.
+
+DEVICES=$(find /sys/class/drm/*/status)
+
+# inspired by /etc/acpd/lid.sh and the function it sources.
+
+displaynum=`ls /tmp/.X11-unix/* | sed s#/tmp/.X11-unix/X##`
+display=":$displaynum.0"
+export DISPLAY=":$displaynum.0"
+
+# from https://wiki.archlinux.org/index.php/Acpid#Laptop_Monitor_Power_Off
+export XAUTHORITY=$(ps -C Xorg -f --no-header | sed -n 's/.*-auth //; s/ -[^ ].*//; p')
+
+for i in /sys/class/drm/*/*/status ;
+do
+	status=$(cat $i);
+	connector=${i%/status*};
+	connector=${connector#*-};
+	if [ "$status" == "disconnected" ]; then
+		xset dpms force off
+	elif [ "$status" == "connected" ]; then
+		xset dpms force on
+		if [ "$(xrandr | grep '\*')" = "" ]; then
+			xrandr --output $connector --auto
+		fi
+	fi
+done
