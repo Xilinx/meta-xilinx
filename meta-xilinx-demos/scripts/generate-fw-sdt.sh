0b2107aad85b8da5e19bb28c6d9c8de0dcf0eb29
Altaf Patel <paltafab@amd.com>
2025-07-10 12:55:30 +0530

    meta-xilinx-demos: generate-fw-sdt.sh: Add vck190-pl-bram-gpio-fw
    generate-fw-sdt.sh: Add support for vck190-pl-bram-gpio-fw firmware generation in meta-xilinx-demos
    
    Signed-off-by: Altaf Patel <paltafab@amd.com>
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- meta-xilinx-demos/scripts/generate-fw-sdt.sh
+++ meta-xilinx-demos/scripts/generate-fw-sdt.sh
@@ -5,6 +5,7 @@
 #M# vek280-pl-bram-gpio-fw  recipes-firmware/vek280-pl-bram-uart-gpio-fw/vek280-pl-bram-uart-gpio-fw_1.0-2025.1.bb
 #M# zcu104-pl-vcu-fw        recipes-firmware/zcu104-pl-vcu-fw/zcu104-pl-vcu-fw_1.0-2025.1.bb
 #M# zcu111-pl-bram-gpio-fw  recipes-firmware/zcu111-pl-bram-gpio-fw/zcu111-pl-bram-gpio-fw_1.0-2025.1.bb
+#M# vck190-pl-bram-gpio-fw  recipes-firmware/vck190-pl-bram-gpio-fw/vck190-pl-bram-gpio-fw_1.0-2025.1.bb
 
 this=$(realpath $0)
 
