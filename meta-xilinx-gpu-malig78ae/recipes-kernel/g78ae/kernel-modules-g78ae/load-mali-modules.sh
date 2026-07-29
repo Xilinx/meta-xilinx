028b094b0edce86e5c24a2cefb4719dcd0e335f7
Parth Gajjar <parth.gajjar@amd.com>
2026-07-29 04:20:20 -0700

    kernel-modules-g78ae: Select GPU configuration based on device ID
    - Read the device identifier from /proc/device-tree/device_id
    and select the appropriate GPU partition topology and active
    slice configuration at runtime.
    
    Signed-off-by: Parth Gajjar <parth.gajjar@amd.com>
    Message-ID: <20260729112021.1788554-1-parth.gajjar@amd.com>
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

--- meta-xilinx-gpu-malig78ae/recipes-kernel/g78ae/kernel-modules-g78ae/load-mali-modules.sh
+++ meta-xilinx-gpu-malig78ae/recipes-kernel/g78ae/kernel-modules-g78ae/load-mali-modules.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
+DEVICE_ID=$(cat /proc/device-tree/device_id | tr -d '\0')
+
 modprobe mali_gpu_pm
 modprobe mali_gpu_power
 modprobe mali_gpu_system
-modprobe mali_gpu_assign ptm_config='A:S0:S1:P0:W0'
+
+case "$DEVICE_ID" in
+    xc2vm3654)
+        PTM_CONFIG="A:S0:P0:W0"
+        ACTIVE_SLICES="0x1"
+        ;;
+    xc2ve3858)
+        PTM_CONFIG="A:S0:S1:P0:W0"
+        ACTIVE_SLICES="0x3"
+        ;;
+    *)
+        echo "Unknown device: $DEVICE_ID"
+        exit 1
+        ;;
+esac
+
+modprobe mali_gpu_assign ptm_config="$PTM_CONFIG"
 modprobe mali_gpu_partition_config
 modprobe mali_gpu_partition_control
 modprobe mali_arbiter
 modprobe mali_gpu_resource_group
 
-echo 0x3 > /sys/module/mali_gpu_resource_group/drivers/*/*/arbiter/partitions/partition0/active_slices
+echo "$ACTIVE_SLICES" > /sys/module/mali_gpu_resource_group/drivers/*/*/arbiter/partitions/partition0/active_slices
 echo 0x1 > /sys/module/mali_gpu_resource_group/drivers/*/*/arbiter/partitions/partition0/assigned_access_windows
 
 modprobe mali_gpu_aw
